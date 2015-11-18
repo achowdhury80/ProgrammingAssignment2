c12528e48228da7002d645ab0d013c2030929abe
achowdhury80 <achowdhury80@gmail.com>
2015-11-17 16:16:35 -0800

    Implemented functions to compute inverse of a matrix and cached it

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions accepts matrix and calculate the inverse of the same. 
+## The inverse of the matrix is cached. 
+## The cache value is returned, if the underlying matrix remains unchanged.
+## If the underlying matrix is changed then new inverse matrix is computed and cached for future use.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## The function accepts numeric matrix. 
+## If nothining is passed as argument, it takes an empty matrix as argument
+## The function returns a list
+## The returned list has four function objects
+## the first funtion object in the list, can be used to set the matrix.
+## the second funtion object in the list, can be used to get the matrix.
+## the third funtion object in the list, can be used to set the inverse of the matrix.
+## the fourth funtion object in the list, can be used to get the inverse of underlying matrix.
 
+makeCacheMatrix <- function(x=matrix()){
+	inverse <- NULL
+	set <- function(m){
+		x <<- m
+		inverse <<- NULL
+	}
+	get <- function(){
+		x
+	}
+	getInverse <- function(){
+		inverse
+	}
+	setInverse <- function(inv) {
+		inverse <<- inv
+	}
+	list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## This function returns the inverse of previously saved matrix.
+## If previously computed cached value exists, then cache value is returned.
+## Otherwise, it computes the inverse of the matrxi, save it in cache and returns the computed inverse matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	inverse <- x$getInverse()
+	if(!is.null(inverse)) {
+		message("getting cached data")
+		return(inverse)
+	}
+	x$setInverse(solve(x$get())) 
+	x$getInverse()
 }
